0ae5ebccd55e291258b5d8a737d347f8630edd4f
mpaszkowski <paszkowski.mikolaj98@gmail.com>
2023-12-06 00:36:05 +0100

    day 1 task

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,54 @@
 fun main() {
+    fun getNumPs1(seq: String): Int {
+        var num = ""
+        for (char in seq) {
+            if (Character.isDigit(char)) {
+                num += char
+            }
+        }
+        return 10 * (num[0] - '0') + (num[num.length - 1] - '0')
+    }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (seq in input) {
+            sum += getNumPs1(seq)
+        }
+        return sum
+    }
+
+    val numbers = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun getNumPs2(seq: String): Int {
+        var num = ""
+        var i = 0
+        while (i < seq.length) {
+            for (index in 0 until numbers.size) {
+                if (numbers[index].length + i <= seq.length && seq.substring(
+                        i,
+                        numbers[index].length + i
+                    ) == numbers[index]
+                ) {
+                    num += ('1' + index).toString()
+                    break
+                }
+            }
+            if (Character.isDigit(seq[i])) {
+                num += seq[i]
+            }
+            ++i
+        }
+        return (num[0] - '0') * 10 + (num[num.length - 1] - '0')
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (seq in input) {
+            sum += getNumPs2(seq)
+        }
+        return sum
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
